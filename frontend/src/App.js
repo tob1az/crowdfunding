4efe9119ac8c8159ad39c77f2b91a94028a29339
Serhii Ternovtsov <tserg42@gmail.com>
2024-07-26 01:34:14 +0300

    add UI that can connect to Solana wallet

---
+++ frontend/src/App.js
@@ -0,0 +1,51 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+const App = () => {
+  const [walletAddress, setWalletAddress] = useState(null);
+  const checkIfWalletIsConnected = async () => {
+    try {
+      const { solana } = window;
+      if (solana) {
+        if (solana.isPhantom) {
+          console.log("Phantom wallet found");
+          const response = await solana.connect({
+            onlyIfTrusted: true,
+          });
+          console.log("Connected with public key:",
+            response.publicKey.toString()
+          );
+          setWalletAddress(response.publicKey.toString());
+        } else {
+          alert("Solana object not found! Get a Phantom wallet");
+        }
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const connectWallet = async () => {
+    const { solana } = window;
+    if (solana) {
+      const response = await solana.connect();
+      console.log("Connected with public key:",
+        response.publicKey.toString()
+      );
+      setWalletAddress(response.publicKey.toString());
+    }
+  };
+
+  const renderNotConnectedContainer = () => (
+    <button onClick={connectWallet}>Connect to Wallet</button>
+  );
+  useEffect(() => {
+    const onLoad = async () => {
+      await checkIfWalletIsConnected();
+    }
+    window.addEventListener("load", onLoad);
+    return () => window.removeEventListener("load", onLoad);
+  }, []);
+
+  return <div className="App">{!walletAddress && renderNotConnectedContainer()}</div>;
+}
+
+export default App;
